a2ec741470b351adcd41209384fc50a8803787c4
Dra <dramane.coulibalyd636@gmail.com>
2023-02-06 23:27:53 +0000

    deploy

---
+++ main.js
@@ -0,0 +1,7 @@
+
+btn1.onclick = () => document.body.style.backgroundColor = "blue";
+btn2.onclick = () => document.body.style.backgroundColor = "black";
+btn3.onclick = () => document.body.style.backgroundColor = "green";
+btn4.onclick = () => document.body.style.backgroundColor = "yellow";
+btn5.onclick = () => document.body.style.backgroundColor = "red";
+alertbnt.onclick = ()=> alert("Dracoul is the Best")
