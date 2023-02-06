9ba001480ee00b188a8cb2f7b614af94698c71cc
Dra <dramane.coulibalyd636@gmail.com>
2023-02-06 23:52:14 +0000

    voila

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+const btn1 = document.getElementById("btn1");
 
 btn1.onclick = () => document.body.style.backgroundColor = "blue";
-btn2.onclick = () => document.body.style.backgroundColor = "black";
-btn3.onclick = () => document.body.style.backgroundColor = "green";
+btn2.onclick = () => document.body.style.backgroundColor = "skyblue";
+btn3.onclick = () => document.body.style.backgroundColor = "purple";
 btn4.onclick = () => document.body.style.backgroundColor = "yellow";
 btn5.onclick = () => document.body.style.backgroundColor = "red";
+btn6.onclick = () => document.body.style.backgroundColor = "pink";
 alertbnt.onclick = ()=> alert("Dracoul is the Best")
