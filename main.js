d8d97eacb56f00a7244a00b97224584cbd70e633
Dra <dramane.coulibalyd636@gmail.com>
2023-02-07 00:08:06 +0000

    voila

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const btn1 = document.getElementById("btn1");
 
-btn1.onclick = () => document.body.style.backgroundColor = "blue";
+btn1.onclick = () => document.body.style.backgroundColor = "orange";
 btn2.onclick = () => document.body.style.backgroundColor = "skyblue";
 btn3.onclick = () => document.body.style.backgroundColor = "purple";
 btn4.onclick = () => document.body.style.backgroundColor = "yellow";
